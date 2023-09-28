847985efd7542562edceb1b5cd7d67a23338b643
marson <marsonm985@gmail.com>
2023-09-28 15:54:33 -0400

    point calc

---
+++ src/Point.java
@@ -0,0 +1,71 @@
+public class Point {
+
+
+    private int x;
+    private int y;
+
+
+
+
+
+
+
+
+    public Point(int x , int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Point(int samePoint) {
+        x = samePoint;
+        samePoint = y;
+    }
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+    public int getX() {
+        return x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+    public void setX(int newX) {
+        x = newX;
+    }
+    public void setY(int newY) {
+        y = newY;
+    }
+
+
+    public String coordinate() {
+        return "(" + x + "," + y + ")";
+
+
+    }
+    public String quadrant() {
+        String quadrant;
+        if (x < 0 && y > 0) {
+            return "II";
+        } else if (x == 0 && y == 0) {
+            return "origin";
+        } else if (x == 0 && y != 0) {
+            return "x-axis";
+        } else if (x != 0 && y == 0) {
+            return "y-axis";
+        } else if (x > 0 && y > 0) {
+            return "I";
+        } else if (x > 0 && y < 0) {
+            return "III";
+        } else if (x < 0 && y < 0) {
+            return "IV";
+        }
+        return quadrant();
+    }
+
+
+
+
+}
+
